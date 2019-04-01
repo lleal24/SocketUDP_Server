1c85266f5aa004753bd035acde293f866b1329e6
lleal24 <lorenaleal24@gmail.com>
2019-03-31 20:06:18 -0500

    Se recibe el mensaje del cliete, uno a uno este se convierte a entero, y luego cada numero es guardado en una posicion de un array, al igual que en el cliente se tieneen cuenta un contador, al llegar al numero 8 este opera entre el vector que aloja el numero en binario con el vector que tiene el valor por posicion calcula el numero pensado y muestra en consola en resultado

--- src/socketudp_server/SocketUDP_Server.java
+++ src/socketudp_server/SocketUDP_Server.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,31 +28,64 @@ public class SocketUDP_Server {
             DatagramSocket miSocket = new DatagramSocket(9107);
             System.out.print("Servidor iniciado\n");
             //Creacion de buffer con cantidad maxima de memoria
-            byte [] buffer = new byte[1024];
-            
+            byte[] buffer = new byte[1024];
+
+            int[] Clave = {1, 2, 4, 8, 16, 32, 64};
+            int[] numero = new int[7];
+
+            int numPensado = 0;
+
+            int contador = 1;
+
             //Ciclo infinito que mantiene la conexion abierta del server
             //esperando a la peticion del cliente
-            while(true){
-                //Paquete que recibe peticion de cliete
-                //solicita como parametro buffer y la dimencion del buffer
+            while (true) {
+
                 DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
                 //el socket recibe peticion en base a datagramaPacket
                 miSocket.receive(peticion);
-                
-                
-                //impresion del mensaje
+
+                //conversion del paquete Datagram a String 
+                String cadena = new String(peticion.getData(), 0, peticion.getLength());
+                 //   Conversion de cadena a Array separado por comas
+                //String numString[] = cadena.split("");
+                int bite = Integer.parseInt(cadena);
+                numero[contador - 1] = bite;
+                contador++;                
+
+            //impresion del mensaje
                 System.out.println("IP: " + peticion.getAddress());
                 System.out.println("Puerto: " + peticion.getPort());
                 //La data llega en formato byte es necesario convertila a String
-                System.out.println("Mensaje: " + new String(peticion.getData(),0,peticion.getLength()));
+                System.out.println("Mensaje: " + cadena);
+                
+
+                for (int i = 0; i < numero.length; i++) {
+                    System.out.print(numero[i]);
+                }
+                
+                if(contador==8){
+                     for(int i=0; i<numero.length; i++){
+                	numPensado+=numero[i]*Clave[i];
+                }
+                
+                System.out.println("\nNumero pensado " + numPensado);
+                //
+                    String enviar = Integer.toString(numPensado);
+                    buffer = enviar.getBytes();
+                    DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length, peticion.getAddress(), peticion.getPort()); 
+                    
+                }
+               
+                
+
                 
             }
-            
         } catch (SocketException ex) {
             Logger.getLogger(SocketUDP_Server.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(SocketUDP_Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
